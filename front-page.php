da41e41eae87038b5d07b050559f42b2b2af2dbb
Ruslan <matusevich@teamidea.ru>
2021-05-02 00:59:36 +0300

    feat: add initial layout and slider

--- front-page.php
+++ front-page.php
@@ -1,3 +1,23 @@
 <?php get_header(); ?>
- <main>HOHOOHO</main>
+ <main>
+   <section>
+     <div class="swiper-container front-slider">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+          <img src="https://www.boconcept.com/on/demandware.static/-/Library-Sites-BoConceptSharedLibrary/default/dw4c440519/images/activities/apr-jun-21/1659405-hero-L.jpg" alt="">
+        </div>
+        <div class="swiper-slide">
+          <img src="https://www.boconcept.com/on/demandware.static/-/Library-Sites-BoConceptSharedLibrary/default/dwa1feb7ca/images/activities/apr-jun-21/1481897-hero-L.jpg" alt="">
+        </div>
+        <div class="swiper-slide">
+          <img src="https://www.boconcept.com/on/demandware.static/-/Library-Sites-BoConceptSharedLibrary/default/dw5922766b/images/activities/apr-jun-21/1659409-hero-L.jpg" alt="">
+        </div>
+      </div>
+      <div class="swiper-pagination"></div>
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+    </div>
+   </section>
+
+ </main>
 <?php get_footer(); ?>
