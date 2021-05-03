489296670a6489985b0a1898d86ff839bb495f24
Ruslan <matusevich@teamidea.ru>
2021-05-03 18:08:24 +0300

    update front-page

--- front-page.php
+++ front-page.php
@@ -26,42 +26,50 @@ $slider = get_field('main-slider', 8);
     </div>
   </section>
 
-  <section>
-    <div class="block-info">
-      <?php
-      $block_info = get_field('block_info', 8);
-      ?>
+  <section class="main-feature">
+    <div class="main-feature-container">
+      <?php $block_info = get_field('block_info', 8); ?>
       <div class="left-block">
-        <img src="<?= $block_info['left_block']['left_image']['url']; ?>" alt="">
-        <p><?= $block_info['left_block']['left_headline'] ?></p>
-        <p><?= $block_info['left_block']['left_description'] ?></p>
-        <?php if ($block_info['left_block']['left_link']) { ?>
-          <a href="<?= $block_info['left_block']['left_link']['url'] ?>">
-            <?= $block_info['left_block']['left_link']['title'] ?>
-          </a>
-        <?php } ?>
+        <div class="img-wrapper">
+          <img src="<?= $block_info['left_block']['left_image']['url']; ?>" alt="">
+        </div>
+        <div class="info-content">
+          <h2><?= $block_info['left_block']['left_headline'] ?></h2>
+          <p><?= $block_info['left_block']['left_description'] ?></p>
+          <?php if ($block_info['left_block']['left_link']) { ?>
+            <a href="<?= $block_info['left_block']['left_link']['url'] ?>">
+              <?= $block_info['left_block']['left_link']['title'] ?>
+            </a>
+          <?php } ?>
+        </div>
       </div>
       <div class="right-block">
-        <img src="<?= $block_info['right_block']['right_image']['url']; ?>" alt="">
-        <p><?= $block_info['right_block']['right_headline'] ?></p>
-        <p><?= $block_info['right_block']['right_description'] ?></p>
-        <?php if ($block_info['right_block']['right_link']) { ?>
-          <a href="<?= $block_info['right_block']['right_link']['url'] ?>">
-            <?= $block_info['right_block']['right_link']['title'] ?>
-          </a>
-        <?php } ?>
+        <div class="img-wrapper">
+          <img src="<?= $block_info['right_block']['right_image']['url']; ?>" alt="">
+        </div>
+        <div class="info-content">
+          <h2><?= $block_info['right_block']['right_headline'] ?></h2>
+          <p><?= $block_info['right_block']['right_description'] ?></p>
+          <?php if ($block_info['right_block']['right_link']) { ?>
+            <a href="<?= $block_info['right_block']['right_link']['url'] ?>">
+              <?= $block_info['right_block']['right_link']['title'] ?>
+            </a>
+            <?php } ?>
+        </div>
       </div>
     </div>
   </section>
 
   <section>
-    <div class="general_info">
-      <?php
-      $general_info = get_field('general_info', 8);
-      ?>
-      <p><?= $general_info['subheadline'] ?></p>
-      <p><?= $general_info['headline'] ?></p>
-      <p><?= $general_info['description'] ?></p>
+    <div class="container-l">
+      <div class="general_info">
+        <?php
+        $general_info = get_field('general_info', 8);
+        ?>
+        <p><?= $general_info['subheadline'] ?></p>
+        <p><?= $general_info['headline'] ?></p>
+        <p><?= $general_info['description'] ?></p>
+      </div>
     </div>
   </section>
 </main>
