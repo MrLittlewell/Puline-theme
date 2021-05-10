e7dbe143a78c45c294e38738b1da103bda3ce2e2
den <justjudobest@mail.ru>
2021-05-10 14:09:15 +0200

    footer styles

--- footer.php
+++ footer.php
@@ -1,32 +1,55 @@
 <footer>
-  <?php wp_nav_menu([
-    'menu' => 'header_and_footer_menu',
-    'theme_location' => 'header_and_footer_menu',
-  ]);
-  $address = get_field('address', 'option');
-  $email = get_field('email', 'option');
-  $phone = get_field('phone', 'option');
-  $social_networks = get_field('social_networks', 'option');
-  ?>
-  <div class="footer-information">
-    <div> <?= $address ?> </div>
-    <div> <?= $email ?> </div>
-    <div> <?= $phone ?> </div>
-    <div>
-      <?php if ($social_networks) : ?>
-        <?php foreach ($social_networks as $social) : ?>
-          <a href="<?= $social['link']['url'] ?>">
-            <img src="<?= $social['image_social']['url']   ?>" alt="">
-          </a>
-        <?php endforeach; ?>
-      <?php endif; ?>
-    </div>
+    <div class="container-l">
+        <div class="footer-menu">
+          <?php wp_nav_menu([
+            'menu' => 'header_and_footer_menu',
+            'theme_location' => 'header_and_footer_menu',
+          ]); ?>
+        </div>
 
-  </div>
+      <?php
+      $address = get_field('address', 'option');
+      $email = get_field('email', 'option');
+      $phone = get_field('phone', 'option');
+      $social_networks = get_field('social_networks', 'option');
+      ?>
+        <div class="footer-information">
+            <div class="contact-info">
+                <div> Контактный адрес </div>
+                <div> <?= $address ?> </div>
+            </div>
+            <div class="contact-info">
+                <div> Контактная почта </div>
+                <div> <?= $email ?> </div>
+            </div>
+            <div class="contact-info">
+                <div> Контактный телефон </div>
+                <div> <?= $phone ?> </div>
+            </div>
+        </div>
+        <div class="footer-social-image">
+
+          <?php if ($social_networks) : ?>
+            <?php foreach ($social_networks as $social) : ?>
+                  <div>
+                      <a href="<?= $social['link']['url'] ?>">
+                          <img src="<?= $social['image_social']['url'] ?>" alt="">
+                      </a>
+                  </div>
+            <?php endforeach; ?>
+          <?php endif; ?>
+
+        </div>
+    </div>
+        <div class="Copyright">Copyright &copy; <?php echo date("Y"); ?>. &nbsp;
+            <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>.
+            Все права защищены. <br/>
+        </div>
+
 </footer>
 <div>
   <?php wp_footer(); ?>
 
-  </body>
+    </body>
 
-  </html>
+    </html>
